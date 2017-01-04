eb01f2d295bd1b41322cfe353dfb1dfb9aeb583d
maxim.s <maxim.shayakhmetov@digitalalchemy.com.au>
2017-01-04 14:46:32 +1100

    finilized folders creation

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,35 +25,36 @@ class chroot_ssh
 if $::drbd_node_status == 'Primary' {
   notify {"this is primary node, creating sftp folders":}
   $folders.each | $username, $folders_for_user | {
-      notify {"####### $folders_for_user is created for $username #####":}
+      notify {"$folders_for_user is created for $username":}
       $folders_for_user.each | String $dir_name |
       {
-        notify {"current dir is $dir_name":}
+        file {"${dir_name}/inbox/" :
+          path => "${dir_name}/inbox/",
+          ensure => $ensure,
+          owner => $username,
+          group => $group,
+          mode => $mode,   
+          }
+        file {"${dir_name}/outbox/" :
+          path => "${dir_name}/outbox/",
+          ensure => $ensure,
+          owner => $username,
+          group => $group,
+          mode => $mode,  
+          }
+      $parent = regsubst($dir_name, '/[^/]*/?$', '')
+      if ($parent != $dir_name) and ($parent != '') 
+      {
+        exec { "create parent directory $parent for $dir_name": 
+        command => "/bin/mkdir -p $parent",
+        creates => "$parent",
+        before => File[$dir_name]
+        }
       }
+    }
   }
- #keys($dir_list).each | String $client_env |  
- #{
- #  $dir_list[$client_env][directory].each|String $dir_name |
- #  {
- #    file {$dir_name :
- #      path => $dir_name,
- #      ensure => $ensure,
- #      owner => $dir_list[$client_env][owner],
- #      group => $group,
- #      mode => $mode,    
- #  }
- # 
- #  $parent = regsubst($dir_name, '/[^/]*/?$', '')
- #  if ($parent != $dir_name) and ($parent != '') {
- #    exec { "create parent directory $parent for $dir_name":
- #      command => "/bin/mkdir -p $parent",
- #      creates => "$parent",
- #      before => File[$dir_name]
- #    }
- #   }
- #  }
- #  }
-  }
+
+}
 else {
   notify {"this is not a primary node, will not create DRBD folders":}   
   }

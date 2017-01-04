5db234e2d13c70c3e2703352c98678850888fbe2
maxim.s <maxim.shayakhmetov@digitalalchemy.com.au>
2017-01-04 13:19:40 +1100

    typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,8 +25,8 @@ class chroot_ssh
 if $::drbd_node_status == 'Primary' {
   notify {"this is primary node, creating sftp folders":}
   $folders.each | $username, $folders_list | {
-      notify { "Defined folders":
-      message => "####### $folders_list is created for $username user ##########"
+      notify { "Defined folders for $username":
+      message => "####### $folders_list is created for $username #####"
     }
   }
  #keys($dir_list).each | String $client_env |  
